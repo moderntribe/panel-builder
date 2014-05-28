448e449c0a87140aa52597f3d9fb3c107600a344
Jonathan Brinley <jbrinley@flightless.us>
2014-05-27 23:11:29 -0400

    select box field type - updates #27723

---
+++ ModularContent/Fields/Select.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace ModularContent\Fields;
+
+
+class Select extends Field {
+	protected $options = array();
+	protected $options_cache = NULL;
+
+	public function __construct( $args = array() ){
+		$this->defaults['options'] = $this->options;
+		parent::__construct($args);
+	}
+
+	public function render_field() {
+		$options = array();
+		$option = '<option value="%s" %s>%s</option>';
+		foreach ( $this->get_options() as $key => $label ) {
+			$selected = sprintf('<# if ( data.fields.%s == "%s" ) { #> selected="selected" <# } #> ', $this->name, esc_js($key));
+			$options[] = sprintf($option, esc_attr($key), $selected, esc_html($label));
+		}
+
+		$select = sprintf('<select name="%s">%s</select>', $this->get_input_name(), implode("\n", $options));
+		echo $select;
+	}
+
+	protected function get_options() {
+		if ( isset($this->options_cache) ) {
+			return $this->options_cache;
+		}
+		if ( empty($this->options) ) {
+			return array();
+		}
+		if ( is_callable($this->options) ) {
+			$this->options_cache = call_user_func($this->options);
+		} else {
+			$this->options_cache = $this->options;
+		}
+		return $this->options_cache;
+	}
+} 
