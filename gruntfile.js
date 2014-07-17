a862d85328bf982636d779e9f397079d1bff617e
Jens Ahrengot Boddum <ahrengot@gmail.com>
2014-07-17 16:30:15 +0200

    Modify jshint rules and use jshint-stylish

--- gruntfile.js
+++ gruntfile.js
@@ -93,7 +93,8 @@ module.exports = function(grunt) {
 		},
 		jshint: {
 			options: {
-				jshintrc: '.jshintrc'
+				jshintrc: '.jshintrc',
+				reporter: require('jshint-stylish')
 			},
 			all: [
 				'Gruntfile.js',
