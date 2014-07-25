ce32f5024b5dc3f1a879975f9af65a9ed2d7c502
Jens Ahrengot Boddum <ahrengot@gmail.com>
2014-07-25 15:24:57 +0200

    Use CSS to select the panel title instead of a css class

--- ModularContent/Fields/Field.php
+++ ModularContent/Fields/Field.php
@@ -106,11 +106,7 @@ abstract class Field {
 
 	protected function render_label() {
 		if ( !empty($this->label) ) {
-			$css_class = "panel-input-label";
-			if ( strtolower( $this->label ) === "title" ) {
-				$css_class .= " panel-input-label-title";
-			}
-			printf('<label class="%1s">%2s</label>', $css_class, $this->label);
+			printf('<label class="panel-input-label">%s</label>', $this->label);
 		}
 	}
 
