7e1efd4e2698709d6e25777e43df1802b4852535
Jens Ahrengot Boddum <ahrengot@gmail.com>
2014-08-28 23:47:15 +0200

    Replace placeholder icons with real icons in panel builder

--- assets/scripts/js/fields-combined.js
+++ assets/scripts/js/fields-combined.js
@@ -1,5 +1,5 @@
 /**
- * Auto-concatenaed on 2014-08-07 based on files in assets/scripts/js/fields
+ * Auto-concatenaed on 2014-08-28 based on files in assets/scripts/js/fields
  */
 
 (function($) {
