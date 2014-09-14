c425e0347d71cf91b6e29425641cf1f0ee008d90
Peter Chester <peter@tri.be>
2014-09-14 10:59:54 -0700

    Add language support to Title field.

--- ModularContent/Fields/Title.php
+++ ModularContent/Fields/Title.php
@@ -4,10 +4,11 @@ namespace ModularContent\Fields;
 use ModularContent\Panel;
 
 class Title extends Text {
-	protected $label = 'Title';
+	protected $label = '';
 	protected $name = 'title';
 
 	public function __construct( $args = array() ){
+		$this->label = __( 'Title', 'modular-content' );
 		parent::__construct($args);
 	}
 }
