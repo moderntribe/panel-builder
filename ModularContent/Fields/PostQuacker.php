eeb745835e1e0359402fab9ce68bf945e7a7bb96
Jonathan Brinley <jbrinley@flightless.us>
2014-10-13 22:18:40 -0400

    filter out invalid post types in post/quacker meta box

--- ModularContent/Fields/PostQuacker.php
+++ ModularContent/Fields/PostQuacker.php
@@ -115,6 +115,7 @@ class PostQuacker extends Field {
 		$post_types = get_post_types(array('has_archive' => TRUE, 'public' => TRUE), 'objects', 'and');
 		$post_types['post'] = get_post_type_object('post'); // posts are special
 		unset($post_types['landing_page']); // because, really, why would you?
-		return apply_filters('panels_query_post_type_options', $post_types, $this );
+		$post_types = apply_filters('panels_query_post_type_options', $post_types, $this );
+		return array_filter( $post_types );
 	}
 }
