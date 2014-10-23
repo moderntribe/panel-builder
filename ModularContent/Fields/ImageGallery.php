02827d6b922ad725c6f602ab071330ec7c8edac4
Jonathan Brinley <jbrinley@flightless.us>
2014-10-22 22:47:33 -0400

    pass image IDs to front-end templates from gallery field

--- ModularContent/Fields/ImageGallery.php
+++ ModularContent/Fields/ImageGallery.php
@@ -12,4 +12,14 @@ class ImageGallery extends Field {
 		$input_value = $this->get_input_value();
 		include( \ModularContent\Plugin::plugin_path( 'admin-views/field-gallery.php' ) );
 	}
+
+	public function get_vars( $data, $panel ) {
+		/*
+		 * We have thumbnails stored with the IDs for quick loading in the admin,
+		 * but we only want to send IDs to the front-end
+		 */
+		$images = parent::get_vars( $data, $panel );
+		$ids = wp_list_pluck( $images, 'id' );
+		return $ids;
+	}
 }
