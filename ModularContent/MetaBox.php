a18392e5aba7c3197dd9866875265f1eaf613754
Daniel Dvorkin <daniel@mzaweb.com>
2015-03-31 01:11:54 -0300

    Introduce 'submission_to_json_collection' filter

--- ModularContent/MetaBox.php
+++ ModularContent/MetaBox.php
@@ -173,6 +173,9 @@ class MetaBox {
 			}
 		}
 		$collection = PanelCollection::create_from_array( array('panels' => $panels) );
+
+		$collection = apply_filters( 'submission_to_json_collection', $collection, $submission );
+
 		return json_encode($collection);
 	}
 
