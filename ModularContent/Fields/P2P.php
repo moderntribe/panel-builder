3d43f67d98ee9860e66955c01b9c79d250493c5b
Daniel Dvorkin <daniel@mzaweb.com>
2015-09-02 16:35:25 -0300

    Implement get_value_for_api for P2P

--- ModularContent/Fields/P2P.php
+++ ModularContent/Fields/P2P.php
@@ -63,4 +63,27 @@ class P2P extends Field {
 		}
 		return $connected;
 	}
+
+	public function get_vars_for_api( $data, $panel ) {
+		if ( ! function_exists( 'p2p_type' ) // we need P2P to filter the query appropriately
+		     || ! is_singular() // we need an is_singular() context to find related posts
+		) {
+			return array(
+				$this->name = array(),
+			);
+		}
+		$context_id = get_queried_object_id();
+		/** @var $type \P2P_Connection_Type */
+		$type      = p2p_type( $this->connection_type );
+		$connected = array();
+		if ( $context_id && $type ) {
+			/** @var $query \WP_Query */
+			$query = @$type->get_connected( $context_id, array( 'posts_per_page' => $this->limit ) );
+			if ( $query ) {
+				$connected = $query->get_posts();
+			}
+		}
+
+		return $connected;
+	}
 } 
