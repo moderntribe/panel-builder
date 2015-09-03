4078e4157c726571fec436606982e3fdd15a14c3
Daniel Dvorkin <daniel@mzaweb.com>
2015-09-03 00:53:22 -0300

    Set correct image size for api value on Image field and add filter

--- ModularContent/Fields/Image.php
+++ ModularContent/Fields/Image.php
@@ -56,7 +56,9 @@ class Image extends Field {
 
 	public function get_vars_for_api( $data, $panel ) {
 
-		$new_data = wp_get_attachment_image_src( $data );
+		$size = apply_filters( 'panels_image_field_size_for_api', $this->size, $this, $data, $panel );
+
+		$new_data = wp_get_attachment_image_src( $data, $size );
 
 		$new_data = apply_filters( 'panels_field_vars_for_api', $new_data, $data, $this, $panel );
 
