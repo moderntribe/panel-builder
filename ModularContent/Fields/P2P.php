d66de0cc54cd2ea1745ab5477776d8741ec6e497
Daniel Dvorkin <daniel@mzaweb.com>
2015-09-02 23:07:38 -0300

    Implement get_vars_for_api for P2P

--- ModularContent/Fields/P2P.php
+++ ModularContent/Fields/P2P.php
@@ -65,25 +65,51 @@ class P2P extends Field {
 	}
 
 	public function get_vars_for_api( $data, $panel ) {
-		if ( ! function_exists( 'p2p_type' ) // we need P2P to filter the query appropriately
-		     || ! is_singular() // we need an is_singular() context to find related posts
-		) {
-			return array(
-				$this->name = array(),
-			);
+		$ids = $this->get_vars_for_api( $data, $panel );
+
+		return array_map( array( $this, 'post_id_to_array' ), $ids );
+	}
+
+	// ToDo: Can the P2P field be a child of Post_List too?
+	protected function post_id_to_array( $post_id ) {
+		if ( empty( $post_id ) ) {
+			return false;
 		}
-		$context_id = get_queried_object_id();
-		/** @var $type \P2P_Connection_Type */
-		$type      = p2p_type( $this->connection_type );
-		$connected = array();
-		if ( $context_id && $type ) {
-			/** @var $query \WP_Query */
-			$query = @$type->get_connected( $context_id, array( 'posts_per_page' => $this->limit ) );
-			if ( $query ) {
-				$connected = $query->get_posts();
-			}
+		$_post = get_post( $post_id );
+		if ( empty( $_post ) ) {
+			return false;
 		}
 
-		return $connected;
+		$data = array(
+			'title'     => '',
+			'content'   => '',
+			'excerpt'   => '',
+			'image'     => 0,
+			'link'      => array(
+				'url'    => '',
+				'target' => '',
+				'label'  => '',
+			),
+			'post_type' => '',
+			'post_id'   => 0,
+		);
+
+		global $post;
+		$post = $_post;
+		setup_postdata( $post );
+		$data['title']     = get_the_title();
+		$data['content']   = get_the_content();
+		$data['excerpt']   = get_the_excerpt();
+		$data['image']     = get_post_thumbnail_id();
+		$data['link']      = array(
+			'url'    => get_permalink(),
+			'target' => '',
+			'label'  => $data['title'],
+		);
+		$data['post_type'] = $post->post_type;
+		$data['post_id']   = $post->ID;
+		wp_reset_postdata();
+
+		return $data;
 	}
 } 
