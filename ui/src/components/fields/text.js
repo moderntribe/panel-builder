b5577f06137e4db83d5c0023c45c996a80d330e9
Graeme Hoffman <graeme@graemehoffman.com>
2016-05-04 11:55:12 +0200

    First pass at styling for input text field

--- ui/src/components/fields/text.js
+++ ui/src/components/fields/text.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 
+import styles from './text.pcss';
+
 class Text extends Component {
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			input_value: '',
+		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			input_value: e.target.value
+		})
+		return true;
+	}
+
 	render() {
 		// graeme you are going to want to use clasnames module and grab styles from the scss in the old assets folder. check the panel.pcss file.
+		// className={styles.main}
 		return (
-			<div className="panel-input input-name-nav_title input-type-text">
-				<label className="panel-input-label">{this.props.label}</label>
-				<span className="panel-input-field">
-					<input type="text" name={this.props.name} value="" size="40" />
+			<div className={ styles.panelInput }>
+				<label className={ styles.panelInputLabel }>{this.props.label}</label>
+				<span className={ styles.panelInputField }>
+					<input type="text" name={this.props.name} value="" size="40" value={this.state.input_value} onChange={ (e) => { return this.handleChange (e) }} />
 				</span>
-				<p className="description panel-input-description">{this.props.description}</p>
+				<p className={ styles.panelInputDescription }>{this.props.description}</p>
 			</div>
 		);
 	}
