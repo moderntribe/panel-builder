35d1901994654416ae797f82d56074f8984d32ca
Graeme Hoffman <graeme@graemehoffman.com>
2016-05-11 15:52:27 +0200

    Fixes to text field after npm run lint

--- ui/src/components/fields/text.js
+++ ui/src/components/fields/text.js
@@ -3,23 +3,23 @@ import React, { Component, PropTypes } from 'react';
 import styles from './text.pcss';
 
 class Text extends Component {
-	constructor (props) {
+	constructor(props) {
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleChange() {
-		//code to connect to actions that execute on redux store
+		// code to connect to actions that execute on redux store
 	}
 
 	render() {
 		return (
-			<div className={ styles.panelInput }>
-				<label className={ styles.panelInputLabel }>{this.props.label}</label>
-				<span className={ styles.panelInputField }>
+			<div className={styles.panelInput}>
+				<label className={styles.panelInputLabel}>{this.props.label}</label>
+				<span className={styles.panelInputField}>
 					<input type="text" name={this.props.name} value="" size="40" onChange={this.handleChange} />
 				</span>
-				<p className={ styles.panelInputDescription }>{this.props.description}</p>
+				<p className={styles.panelInputDescription}>{this.props.description}</p>
 			</div>
 		);
 	}
