308d8a7470e5e27eb9d7ff6d9a036719bc87645c
Graeme Hoffman <graeme@graemehoffman.com>
2016-05-11 16:22:10 +0200

    Tweaks based on PR notes - Use stateless function not inherited class - Remove prefix from classnames - Added JSON to blueprint - Removed the unused vars from prop declaration

--- ui/src/components/fields/html.js
+++ ui/src/components/fields/html.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
-
 import styles from './html.pcss';
 
-class HTML extends Component {
-
-	render() {
-		return (
-			<div className={ styles.panelHTML }>
-				<div className={ styles.panelHTMLDescription } dangerouslySetInnerHTML={{__html: this.props.description}} ></div>
-			</div>
-		);
-	}
-}
+const HTML = (props) => (
+	<div className={ styles.panel }>
+		<div className={ styles.description } dangerouslySetInnerHTML={{ __html: props.description }} ></div>
+	</div>
+);
 
 HTML.propTypes = {
-	label: React.PropTypes.string,
-	name: React.PropTypes.string,
 	description: React.PropTypes.string,
-	strings: React.PropTypes.array,
-	default: React.PropTypes.string,
 };
 
 HTML.defaultProps = {
-	label: '',
-	name: '',
 	description: '',
-	strings: [],
-	default: '',
 };
+
 
 export default HTML;
