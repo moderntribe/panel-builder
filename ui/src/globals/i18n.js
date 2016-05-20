6944dd1617343a06ac7c93a463d950f75af481b7
Graeme Hoffman <graeme@graemehoffman.com>
2016-05-20 15:43:01 +0200

    Remove console logs from image gallery testing

--- ui/src/globals/i18n.js
+++ ui/src/globals/i18n.js
@@ -4,7 +4,3 @@ export const I18N = window.ModularContentI18n || I18N_DEFAULTS;
 export const FIELDS_I18N = I18N.fields || {};
 export const IMAGE_I18N = FIELDS_I18N.image || {};
 export const IMAGE_GALLERY_I18N = FIELDS_I18N.image_gallery || {};
-
-console.log("I18N",I18N)
-console.log("window.ModularContentI18n",window.ModularContentI18n)
-console.log("I18N_DEFAULTS",I18N_DEFAULTS)
