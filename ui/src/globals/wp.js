7a763df9d9731b6b76e5fb1461884826cf83ada6
Sam Estok <sam@tri.be>
2016-05-25 13:35:27 -0700

    Renoves plupload, patches image field to be simple field, updates build flow, implements react select plus, patches tests.

--- ui/src/globals/wp.js
+++ ui/src/globals/wp.js
@@ -1,9 +1,7 @@
 
 const wp = window.wp || {};
-const plupload = window.plupload || {};
 
 export const wpMedia = wp.media || null;
-export const PlUploader = plupload.Uploader || null;
 export const tinyMCE = window.tinyMCE || null;
 export const tinyMCEPreInit = window.tinyMCEPreInit || null;
 export const switchEditors = window.switchEditors || null;
