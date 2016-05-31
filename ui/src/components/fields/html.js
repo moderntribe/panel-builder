75e87a8cb41018ada814adcfffcfddc1a50e7fca
Graeme Hoffman <graeme@graemehoffman.com>
2016-05-31 19:25:12 +0200

    HTML Field - more consistent target class names

--- ui/src/components/fields/html.js
+++ ui/src/components/fields/html.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from './html.pcss';
+import classNames from 'classnames';
 
-const HTML = (props) => (
-	<div className={styles.panel}>
-		<div className={styles.description} dangerouslySetInnerHTML={{ __html: props.description }} ></div>
+const HTML = (props) => {
+	const descriptionStyles = classNames({
+		[styles.description]: true,
+		'panel-field-description': true,
+	});
+	const fieldStyles = classNames({
+		[styles.field]: true,
+		'panel-field': true,
+	});
+	return (
+	<div className={fieldStyles}>
+		<div className={descriptionStyles} dangerouslySetInnerHTML={{ __html: props.description }} ></div>
 	</div>
-);
+	);
+};
 
 HTML.propTypes = {
 	description: React.PropTypes.string,
