49d82274711b449823ce225fefb9c3f45db42fd3
speerface <aaron@tri.be>
2017-05-09 15:06:52 -0500

    Add ColorPicker field to panels

---
+++ ModularContent/Fields/ColorPicker.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace ModularContent\Fields;
+
+/**
+ * Class Image
+ *
+ * @package ModularContent\Fields
+ *
+ * An image field.
+ *
+ * Note: this depends on the Attachment Helper library.
+ *
+ * The image is stored in the field as an attachment ID.
+ */
+class ColorPicker extends Field {
+
+	protected $default = 0;
+
+	protected $default_swatches = [
+		'#4a7ef2',
+		'#3c73ef',
+		'#3b4664',
+		'#5cb85c',
+		'#d15e61',
+		'#ffa700',
+		'#eee',
+	];
+
+	protected $swatches = [];
+	protected $input_active = false;
+
+	/**
+	 * @param array $args
+	 *
+	 * Usage example:
+	 *
+	 * $field = new ColorPicker( array(
+	 *   'label' => __('Background Color'),
+	 *   'name' => 'background-color',
+	 *   'description' => __( 'The color to use as the background.' ),
+	 *   'swatches' => [ '#000000', '#fcfcfc' ],
+	 *   'input_active' => false,
+	 * ) );
+	 */
+	public function __construct( $args = [] ) {
+		$this->defaults['strings']     = [
+			'input.placeholder' => __( 'enter hex code', 'modular-content' ),
+		];
+		$this->defaults['input_active'] = $this->input_active;
+		$this->defaults['swatches']     = isset( $args['swatches'] ) ? $args['swatches'] : apply_filters( 'panels_default_color_picker_swatches', $this->default_swatches );
+		parent::__construct( $args );
+	}
+
+	public function get_blueprint() {
+		$blueprint                 = parent::get_blueprint();
+		$blueprint['swatches']     = $this->swatches;
+		$blueprint['input_active'] = $this->input_active;
+
+		return $blueprint;
+	}
+
+	/**
+	 * Massage submitted data before it's saved.
+	 *
+	 * @param mixed $data
+	 * @return int
+	 */
+	public function prepare_data_for_save( $data ) {
+		return (int) parent::prepare_data_for_save( $data );
+	}
+}
---
+++ tests/codeception/integration/Fields/Color_Picker_Test.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ModularContent\Fields;
+
+use Codeception\TestCase\WPTestCase;
+
+class Color_Picker_Test extends WPTestCase {
+
+	public function test_blueprint() {
+		$label       = __CLASS__ . '::' . __FUNCTION__;
+		$name        = __FUNCTION__;
+		$description = __FUNCTION__ . ':' . __LINE__;
+		$default     = __LINE__;
+		$swatches    = [ '#CCCCCC' ];
+		$field       = new ColorPicker( [
+			'label'        => $label,
+			'name'         => $name,
+			'description'  => $description,
+			'default'      => $default,
+			'swatches'     => $swatches,
+			'input_active' => false,
+		] );
+
+		$blueprint = $field->get_blueprint();
+
+		$expected = [
+			'type'         => 'ColorPicker',
+			'label'        => $label,
+			'name'         => $name,
+			'description'  => $description,
+			'strings'      => [
+				'input.placeholder' => 'enter hex code',
+			],
+			'default'      => $default,
+			'swatches'     => $swatches,
+			'input_active' => false,
+		];
+
+		$this->assertEquals( $expected, $blueprint );
+	}
+}
