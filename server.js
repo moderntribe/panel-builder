f7e4e6a373c13f81e885247f1dc9f2b868b5f97a
Sam Estok <sam@tri.be>
2017-05-27 09:49:49 -0700

    Accommodate new security reqs in some browsers to keep HMR working.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
 	publicPath: config.output.publicPath,
 	hot: true,
-	historyApiFallback: true
+	historyApiFallback: true,
+	headers: { 'Access-Control-Allow-Origin': '*' },
 }).listen(3000, 'localhost', function (err, result) {
 	if (err) {
 		return console.log(err);
