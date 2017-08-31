bb9fe545e561cf84b1aa32f980d8aeabfd7d53eb
Samuel Estok <sam@tri.be>
2017-08-30 21:38:02 -0700

    Adds picker types to args for color picker field

--- ModularContent/Fields/Color_Picker.php
+++ ModularContent/Fields/Color_Picker.php
@@ -16,6 +16,7 @@ class Color_Picker extends Field {
 	protected $default_swatches = [];
 
 	protected $swatches     = [];
+	protected $picker_type  = 'BlockPicker';
 	protected $input_active = false;
 	protected $allow_clear  = false;
 
@@ -29,7 +30,8 @@ class Color_Picker extends Field {
 	 *   'name' => 'background-color',
 	 *   'description' => __( 'The color to use as the background.' ),
 	 *   'swatches' => [ '#000000', '#fcfcfc' ],
-	 *   'input_active' => false, // if true, displays a text input to define a custom swatch in the field
+	 *   'picker_type' => 'BlockPicker', // supported types AlphaPicker, BlockPicker, ChromePicker, CirclePicker, CompactPicker, GithubPicker, HuePicker, MaterialPicker, PhotoshopPicker, SketchPicker, SliderPicker, SwatchesPicker, TwitterPicker. More info at https://casesandberg.github.io/react-color/
+	 *   'input_active' => false, // if true, displays a text input to define a custom swatch in the field. only applies to some picker types, please check https://casesandberg.github.io/react-color/ for details
 	 *   'allow_clear' => false,
 	 * ) );
 	 */
@@ -38,6 +40,7 @@ class Color_Picker extends Field {
 			'input.placeholder' => __( 'Enter Hex Code', 'modular-content' ),
 		];
 		$this->defaults['input_active'] = $this->input_active;
+		$this->defaults['picker_type']  = $this->picker_type;
 		$this->defaults['allow_clear']  = $this->allow_clear;
 		$this->defaults['swatches']     = isset( $args['swatches'] ) ? $args['swatches'] : apply_filters( 'panels_default_color_picker_swatches', $this->default_swatches );
 		parent::__construct( $args );
@@ -46,6 +49,7 @@ class Color_Picker extends Field {
 	public function get_blueprint() {
 		$blueprint                 = parent::get_blueprint();
 		$blueprint['swatches']     = $this->swatches;
+		$blueprint['picker_type']  = $this->picker_type;
 		$blueprint['input_active'] = $this->input_active;
 		$blueprint['allow_clear']  = $this->allow_clear;
 
