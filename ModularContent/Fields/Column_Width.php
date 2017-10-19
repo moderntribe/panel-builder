39e4868e3223fb0dbbf4340b93087a174363cdef
Samuel Estok <sam@tri.be>
2017-10-18 18:27:21 -0700

    Update docs, nuke ninja forms support.

--- ModularContent/Fields/Column_Width.php
+++ ModularContent/Fields/Column_Width.php
@@ -2,7 +2,6 @@
 
 
 namespace ModularContent\Fields;
-use ModularContent\Panel;
 
 
 /**
@@ -10,12 +9,12 @@ use ModularContent\Panel;
  *
  * @package ModularContent\Fields
  *
- * A group of radio buttons.
+ * A group of radio buttons laid out into an intuitive ui for selecting grid widths a panel/group of panels may take up.
  *
- * $field = new Radio( array(
- *   'label' => __('Pick One'),
+ * $field = new Column_Width( array(
+ *   'label' => __('Column Width'),
  *   'name' => 'my-field',
- *   'description' => __( 'Pick the thing that you pick' )
+ *   'description' => __( 'Select width for this column' )
  *   'columns' => 12
  * ) );
  */
