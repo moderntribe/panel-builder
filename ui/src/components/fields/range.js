efb53e10c2706be2809f3f3ba1ba3613d13bbfb8
Samuel Estok <sam@tri.be>
2018-01-18 17:00:42 -0800

    Finalize ui styles for new range input

--- ui/src/components/fields/range.js
+++ ui/src/components/fields/range.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import Slider from 'rc-slider/lib/Slider';
-import RangeSlider from 'rc-slider/lib/Range';
+import Slider from 'rc-slider';
 import NumericInput from 'react-numeric-input';
+import Tooltip from 'rc-tooltip';
 
 import field from '../shared/field';
 import * as styleUtil from '../../util/dom/styles';
-import styles from './number.pcss';
+import styles from './range.pcss';
+
+const { createSliderWithTooltip } = Slider;
+const RangeSlider = createSliderWithTooltip(Slider.Range);
+const { Handle } = Slider;
+
+const handle = (props) => {
+	const { value, dragging, index, ...restProps } = props; // eslint-disable-line
+	return (
+		<Tooltip
+			prefixCls="rc-slider-tooltip"
+			overlay={value}
+			visible={dragging}
+			placement="top"
+			key={index}
+		>
+			<Handle value={value} {...restProps} />
+		</Tooltip>
+	);
+};
 
 export const Range = (props) => {
 	const handleChange = (value) => {
@@ -33,12 +52,20 @@ export const Range = (props) => {
 						max={props.max}
 						step={props.step}
 						value={props.value[0]}
+						handle={handle}
 						onChange={handleChange}
+						tipFormatter={value => `${value}${props.unit_display}`}
 					/>
 				}
 				{props.handles.length > 1 &&
 					<RangeSlider
-
+						handles={props.handles}
+						min={props.min}
+						max={props.max}
+						step={props.step}
+						value={props.value}
+						onChange={handleChange}
+						tipFormatter={value => `${value}${props.unit_display}`}
 					/>
 				}
 				{props.has_input && props.handles.length === 1 &&
