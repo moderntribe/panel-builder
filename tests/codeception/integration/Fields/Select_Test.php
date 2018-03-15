542a41d85c839d3fcf9bf51a87b6c41bb3844877
speerface <aaron@tri.be>
2018-03-14 22:04:38 -0500

    Add global options and injection args to select

--- tests/codeception/integration/Fields/Select_Test.php
+++ tests/codeception/integration/Fields/Select_Test.php
@@ -11,27 +11,29 @@ class Select_Test extends WPTestCase {
 		$name        = __FUNCTION__;
 		$description = __FUNCTION__ . ':' . __LINE__;
 		$field       = new Select( [
-			'label'       => $label,
-			'name'        => $name,
-			'description' => $description,
-			'default'     => 'second',
-			'options'     => [
+			'label'                  => $label,
+			'name'                   => $name,
+			'description'            => $description,
+			'default'                => 'second',
+			'options'                => [
 				'first'  => 'First Option',
 				'second' => 'Second Option',
 			],
-			'layout'      => 'full',
+			'layout'                 => 'full',
+			'global_options'         => 'google_fonts',
+			'enable_fonts_injection' => true,
 		] );
 
 		$blueprint = $field->get_blueprint();
 
 		$expected = [
-			'type'        => 'Select',
-			'label'       => $label,
-			'name'        => $name,
-			'description' => $description,
-			'strings'     => [],
-			'default'     => 'second',
-			'options'     => [
+			'type'                   => 'Select',
+			'label'                  => $label,
+			'name'                   => $name,
+			'description'            => $description,
+			'strings'                => [],
+			'default'                => 'second',
+			'options'                => [
 				[
 					'label' => 'First Option',
 					'value' => 'first',
@@ -41,7 +43,9 @@ class Select_Test extends WPTestCase {
 					'value' => 'second',
 				],
 			],
-			'layout'      => 'full',
+			'layout'                 => 'full',
+			'global_options'         => 'google_fonts',
+			'enable_fonts_injection' => true,
 		];
 
 		$this->assertEquals( $expected, $blueprint );
