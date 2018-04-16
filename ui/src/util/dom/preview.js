98be246296a001c05b986b759611835874e7e398
Nicholas Smith <nicholaswebdev@gmail.com>
2018-04-16 15:04:59 -0500

    give preview button text screen reader classes

--- ui/src/util/dom/preview.js
+++ ui/src/util/dom/preview.js
@@ -16,23 +16,33 @@ export const createMask = (type = '') => {
 						<span class="${styles.maskEdit}">${UI_I18N['heading.edit_type']}</span><span class="${styles.maskEditing}">${UI_I18N['heading.editing_type']}</span> ${label}
 					</span>
 					<button class="${styles.maskButton} ${styles.maskButtonUp}">
-						<span data-tooltip class="${styles.tooltip}">${UI_I18N['tooltip.panel_up']}</span>
+						<span data-tooltip class="${styles.tooltip}">
+							<span class="${styles.maskScreenReader}">${UI_I18N['tooltip.panel_up']}</span>
+						</span>
 					</button>
 					<button class="${styles.maskButton} ${styles.maskButtonDown}">
-						<span data-tooltip class="${styles.tooltip}">${UI_I18N['tooltip.panel_down']}</span>
+						<span data-tooltip class="${styles.tooltip}">
+							<span class="${styles.maskScreenReader}">${UI_I18N['tooltip.panel_down']}</span>
+						</span>
 					</button>
 					<button class="${styles.maskButton} ${styles.maskButtonDelete}">
-						<span data-tooltip class="${styles.tooltip}">${UI_I18N['tooltip.delete_panel']}</span>
+						<span data-tooltip class="${styles.tooltip}">
+							<span data-tooltip class="${styles.maskScreenReader}">${UI_I18N['tooltip.delete_panel']}</span>
+						</span>
 					</button>
 				</header>
 				<div class="${styles.maskTop} ${styles.maskAdd}">
 					<button class="${styles.maskButtonAdd} ${styles.addPanelAbove}">
-						<span data-tooltip class="${styles.tooltip}">${UI_I18N['tooltip.add_above']}</span>
+						<span data-tooltip class="${styles.tooltip}">
+							<span data-tooltip class="${styles.maskScreenReader}">${UI_I18N['tooltip.add_above']}</span>
+						</span>
 					</button>
 				</div>
 				<div class="${styles.maskBottom} ${styles.maskAdd}">
 					<button class="${styles.maskButtonAdd} ${styles.addPanelBelow}">
-						<span data-tooltip class="${styles.tooltip}">${UI_I18N['tooltip.add_below']}</span>
+						<span data-tooltip class="${styles.tooltip}">
+							<span data-tooltip class="${styles.maskScreenReader}">${UI_I18N['tooltip.add_below']}</span>
+						</span>
 					</button>
 				</div>
 			</div>
