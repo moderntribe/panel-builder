f8654844f1f30e45db0c22abaf68aaf6602b1dbe
Paul <kim.paul.ms@gmail.com>
2019-03-21 02:40:22 +0800

    fix up with store

--- ui/src/common/hoc/withStore.js
+++ ui/src/common/hoc/withStore.js
@@ -1,7 +1,12 @@
+import React from 'react';
 import store from '../../store';
 
 export default () => ( Component ) => {
 	return ( props ) => {
+		const extraProps = {
+			store,
+		};
 
+		return <Component { ...props } { ...extraProps } />;
 	}
 }
