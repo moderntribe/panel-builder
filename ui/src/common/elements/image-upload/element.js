afa2e72fca0de6a2953c2781740cde146830e857
Stephen Page <stephen@tri.be>
2019-03-20 14:53:11 -0500

    Add text, textarea, and image upload elements

---
+++ ui/src/common/elements/image-upload/element.js
@@ -0,0 +1,58 @@
+/**
+ * External dependencies
+ */
+import { React } from 'react';
+import { PropTypes } from 'prop-types';
+import { classNames } from 'classnames';
+import { noop } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { MediaUpload } from '@wordpress/components';
+
+const getImageButton = ( openEvent ) => {
+	if ( attributes.imageUrl ) {
+		return (
+		<img
+			src={ attributes.imageUrl }
+			onClick={ openEvent }
+			className="image"
+		/>
+		);
+	}
+	else {
+		return (
+		<div className="button-container">
+			<Button
+			onClick={ openEvent }
+			className="button button-large"
+			>
+			Pick an image
+			</Button>
+		</div>
+		);
+	}
+};
+
+const ImageUpload = ( {
+	attributes,
+	name
+} ) => (
+	<MediaUpload
+	onSelect={ media => { setAttributes({ imageAlt: media.alt, imageUrl: media.url }); } }
+	name={ name }
+	type="image"
+	value={ attributes.imageID }
+	render={ ({ open }) => getImageButton(open) }
+	/>
+);
+
+
+Input.propTypes = {
+	className: PropTypes.string,
+	value: PropTypes.integer,
+	name: PropTypes.string,
+};
+
+export default ImageUpload;
---
+++ ui/src/common/elements/index.js
@@ -0,0 +1,3 @@
+export { default as Text } from './text/element';
+export { default as ImageUpload } from './image-upload/element';
+export { default as TextArea } from './textarea/element';
