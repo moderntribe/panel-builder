c08c908173142ef1e2de8e9808da829dd467e495
Stephen Page <stephen@tri.be>
2019-03-20 16:31:35 -0500

    Use Freeform code from GB plugin to add WYSIWYG editor

--- ui/src/common/elements/rich-text/element.js
+++ ui/src/common/elements/rich-text/element.js
@@ -1,25 +1,181 @@
 /**
  * External Dependencies
  */
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import { React } from 'react';
+import { PureComponent } from React;
+import { __ } from '@wordpress/i18n';
 import { RichText } from '@wordpress/editor';
+import { BACKSPACE, DELETE, F10 } from '@wordpress/keycodes';
+import PropTypes from 'prop-types';
 
-const Text = ( {
-	className,
-	value,
-	name
-} ) => (
-	<RichText
-		tagName="div"
-		className={ classNames( 'blockpanels__wysiwyg', className ) }
-		palceholder={ __( 'Enter text...', 'custom-block' ) }
-		name={ name }
-		value={ value }
-		keepPlaceholderOnFocus={ true }
-	/>
-);
+function isTmceEmpty( editor ) {
+	// When tinyMce is empty the content seems to be:
+	// <p><br data-mce-bogus="1"></p>
+	// avoid expensive checks for large documents
+	const body = editor.getBody();
+	if ( body.childNodes.length > 1 ) {
+		return false;
+	} else if ( body.childNodes.length === 0 ) {
+		return true;
+	}
+	if ( body.childNodes[ 0 ].childNodes.length > 1 ) {
+		return false;
+	}
+	return /^\n?$/.test( body.innerText || body.textContent );
+}
+
+export default class FreeformEdit extends PureComponent {
+	constructor( props ) {
+		super( props );
+		this.initialize = this.initialize.bind( this );
+		this.onSetup = this.onSetup.bind( this );
+		this.focus = this.focus.bind( this );
+	}
+
+	componentDidMount() {
+		const { baseURL, suffix } = window.wpEditorL10n.tinymce;
+
+		window.tinymce.EditorManager.overrideDefaults( {
+			base_url: baseURL,
+			suffix,
+		} );
+
+		if ( document.readyState === 'complete' ) {
+			this.initialize();
+		} else {
+			window.addEventListener( 'DOMContentLoaded', this.initialize );
+		}
+	}
+
+	componentWillUnmount() {
+		window.addEventListener( 'DOMContentLoaded', this.initialize );
+		wp.oldEditor.remove( `editor-${ this.props.id }` );
+	}
+
+	componentDidUpdate( prevProps ) {
+		const { id, attributes: { content } } = this.props;
+
+		const editor = window.tinymce.get( `editor-${ id }` );
+
+		if ( prevProps.attributes.content !== content ) {
+			editor.setContent( content || '' );
+		}
+	}
+
+	initialize() {
+		const { id } = this.props;
+		const { settings } = window.wpEditorL10n.tinymce;
+		wp.oldEditor.initialize( `editor-${ id }`, {
+			tinymce: {
+				...settings,
+				inline: true,
+				content_css: false,
+				fixed_toolbar_container: `#toolbar-${ id }`,
+				setup: this.onSetup,
+			},
+		} );g
+	}
+
+	onSetup( editor ) {
+		const { attributes: { content }, setAttributes } = this.props;
+		const { ref } = this;
+
+		this.editor = editor;
+
+		if ( content ) {
+			editor.on( 'loadContent', () => editor.setContent( content ) );
+		}
+
+		editor.on( 'blur', () => {
+			setAttributes( {
+				content: editor.getContent(),
+			} );
+			return false;
+		} );
+
+		editor.on( 'keydown', ( event ) => {
+			if ( ( event.keyCode === BACKSPACE || event.keyCode === DELETE ) && isTmceEmpty( editor ) ) {
+				// delete the block
+				this.props.onReplace( [] );
+				event.preventDefault();
+				event.stopImmediatePropagation();
+			}
+
+			const { altKey } = event;
+			/*
+			 * Prevent Mousetrap from kicking in: TinyMCE already uses its own
+			 * `alt+f10` shortcut to focus its toolbar.
+			 */
+			if ( altKey && event.keyCode === F10 ) {
+				event.stopPropagation();
+			}
+		} );
+
+		editor.addButton( 'kitchensink', {
+			tooltip: __( 'More' ),
+			icon: 'dashicon dashicons-editor-kitchensink',
+			onClick: function() {
+				const button = this;
+				const active = ! button.active();
+
+				button.active( active );
+				editor.dom.toggleClass( ref, 'has-advanced-toolbar', active );
+			},
+		} );
+
+		editor.on( 'init', () => {
+			const rootNode = this.editor.getBody();
+
+			// Create the toolbar by refocussing the editor.
+			if ( document.activeElement === rootNode ) {
+				rootNode.blur();
+				this.editor.focus();
+			}
+		} );
+	}
+
+	focus() {
+		if ( this.editor ) {
+			this.editor.focus();
+		}
+	}
+
+	onToolbarKeyDown( event ) {
+		// Prevent WritingFlow from kicking in and allow arrows navigation on the toolbar.
+		event.stopPropagation();
+		// Prevent Mousetrap from moving focus to the top toolbar when pressing `alt+f10` on this block toolbar.
+		event.nativeEvent.stopImmediatePropagation();
+	}
+
+	render() {
+		const { id } = this.props;
+
+		// Disable reason: the toolbar itself is non-interactive, but must capture
+		// events from the KeyboardShortcuts component to stop their propagation.
+		/* eslint-disable jsx-a11y/no-static-element-interactions */
+		return [
+			// Disable reason: Clicking on this visual placeholder should create
+			// the toolbar, it can also be created by focussing the field below.
+			/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
+			<div
+				key="toolbar"
+				id={ `toolbar-${ id }` }
+				ref={ ( ref ) => this.ref = ref }
+				className="freeform-toolbar"
+				onClick={ this.focus }
+				data-placeholder={ __( 'Classic' ) }
+				onKeyDown={ this.onToolbarKeyDown }
+			/>,
+			<div
+				key="editor"
+				id={ `editor-${ id }` }
+				className="wp-block-freeform core-blocks-rich-text__tinymce"
+			/>,
+		];
+		/* eslint-enable jsx-a11y/no-static-element-interactions */
+}
+}
+
 
 Input.propTypes = {
 	className: PropTypes.string,
@@ -27,4 +183,4 @@ Input.propTypes = {
 	name: PropTypes.string,
 };
 
-export default Text;
+export default RichText;
